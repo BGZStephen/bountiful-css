b5b9876e34318b4d1678dff95df10948cb6e3c19
Stephen Wright <sjw948@gmail.com>
2017-08-21 20:41:06 +0100

    added panels and buttons library comonents

---
+++ src/app/modules/website/modules/library/components/library-buttons/library-buttons.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-library-buttons',
+  templateUrl: './library-buttons.component.html',
+  styleUrls: ['./library-buttons.component.scss']
+})
+export class LibraryButtonsComponent {}
---
+++ src/app/modules/website/modules/library/components/library-panels/library-panels.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-library-panels',
+  templateUrl: './library-panels.component.html',
+  styleUrls: ['./library-panels.component.scss']
+})
+export class LibraryPanelsComponent {}
